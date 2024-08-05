85522b09624228daa6fc42eb2017e7b1a4fbe915
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2024-08-06 04:59:43 +0900

    バッタの動きを変更

--- Soul-Hunter/Assets/Scripts/GrasshopperMovement.cs
+++ Soul-Hunter/Assets/Scripts/GrasshopperMovement.cs
@@ -3,22 +3,27 @@ using System.Collections;
 
 public class GrasshopperMovement : WormMovement
 {
+    private Animator anim;
     public float jumpForce = 5.0f; // ジャンプ力
-    public float jumpInterval = 2.0f; // ジャンプ間隔
-    public float jumpNextInterval = 0.5f; // ジャンプ間隔
+    public float minJumpInterval = 3.0f; // ジャンプ間隔の最小値
+    public float maxJumpInterval = 6.0f; // ジャンプ間隔の最大値
+    private bool isJumping = false; // ジャンプ中かどうかのフラグ
 
     protected override void Start()
     {
+        anim = GetComponent<Animator>();
         // 基底クラスのStartメソッドを呼び出す
         base.Start();
-        // ジャンプのコルーチンを開始
-        StartCoroutine(JumpRoutine());
     }
 
     protected override void Move()
     {
-        // 基底クラスのMoveメソッドを呼び出す
-        base.Move();
+        // ジャンプ中のみ移動
+        if (isJumping)
+        {
+            // 基底クラスのMoveメソッドを呼び出す
+            base.Move();
+        }
     }
 
     protected override void CheckDirection()
@@ -29,12 +34,26 @@ public class GrasshopperMovement : WormMovement
 
     private IEnumerator JumpRoutine()
     {
-        while (true)
+        float randomInterval = Random.Range(minJumpInterval, maxJumpInterval); // ランダムなジャンプ間隔を生成
+        yield return new WaitForSeconds(randomInterval); // ジャンプ間隔待つ
+        anim.SetBool("IsIdle", false);
+        anim.SetBool("IsPreparation", true);
+        yield return new WaitForSeconds(1.0f);
+        anim.SetBool("IsPreparation", false);
+        anim.SetBool("IsJump", true);
+        Jump(); // ジャンプ
+        yield return new WaitForSeconds(1.0f);
+        Jump(); // ジャンプ
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            yield return new WaitForSeconds(jumpInterval); // ジャンプ間隔待つ
-            Jump(); // ジャンプ
-            yield return new WaitForSeconds(jumpNextInterval); // ジャンプ間隔待つ
-            Jump(); // ジャンプ
+            isJumping = false; // 地面に接触したらジャンプ終了
+            anim.SetBool("IsJump", false);
+            anim.SetBool("IsIdle", true);
+            StartCoroutine(JumpRoutine()); // ジャンプのコルーチンを開始
         }
     }
 
@@ -42,6 +61,7 @@ public class GrasshopperMovement : WormMovement
     {
         if (rb != null)
         {
+            isJumping = true; // ジャンプ開始
             rb.velocity = new Vector2(rb.velocity.x, jumpForce); // ジャンプ
         }
     }
